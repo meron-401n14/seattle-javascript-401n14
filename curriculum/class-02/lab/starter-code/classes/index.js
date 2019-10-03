5e04e91e2509b065d3b38755a66c3e3a7167d5ee
meron <malemu17@yahoo.com>
2019-10-02 23:45:19 -0700

    toBring

--- curriculum/class-02/lab/starter-code/classes/index.js
+++ curriculum/class-02/lab/starter-code/classes/index.js
@@ -1,6 +1,16 @@
 'use strict';
 
-const VehicleConstructor = require('./vehicle-constructor.js');
+//we set Vehicle equal to a function
+//eventually, we want this to be  a class
+
+// a constructor function - outside a 
+//class ! (behind-the scenes of what a class does )
+
+const VehicleConstructor = require('./vehicle-constructor.js')
+  this.name = name;
+  this.wheels = wheels;
+  
+;
 
 // Implement a car and motorcycle using a Constructor
 const mazda = new VehicleConstructor.Car('Mazda 3');
